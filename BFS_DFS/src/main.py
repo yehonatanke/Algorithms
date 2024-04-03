f7a6220019b10ffd4328d2bb00c3f7e3756f8b20
yehonatanke <150778110+yehonatanke@users.noreply.github.com>
2024-04-03 11:21:52 +0300

    Update main.py

--- BFS_DFS/src/main.py
+++ BFS_DFS/src/main.py
@@ -4,41 +4,6 @@ from dfs import dfs
 from util import display_graph
 
 
-def main2():
-    num_vertices = 6  # Example: creating a graph with 6 vertices
-    g = Graph.create_graph(num_vertices)
-
-    # Display the initial graph
-    print("Initial Graph:")
-    display_graph(g.get_adj_list(), title="Initial Graph")
-
-    # Perform BFS and DFS traversals
-    bfs_traversal = bfs(g.get_adj_list(), 'v_1')
-    dfs_traversal = dfs(g.get_adj_list(), 'v_1')
-
-    # Display BFS traversal on the graph
-    print("\nBFS Traversal:", bfs_traversal)
-    display_graph(g.get_adj_list(), traversal_path=bfs_traversal, title="BFS Traversal")
-
-    # Display DFS traversal on the graph
-    print("\nDFS Traversal:", dfs_traversal)
-    display_graph(g.get_adj_list(), traversal_path=dfs_traversal, title="DFS Traversal")
-
-    # Convert BFS and DFS traversal paths to graphs
-    bfs_graph = Graph()
-    dfs_graph = Graph()
-    for i in range(len(bfs_traversal) - 1):
-        bfs_graph.add_edge(bfs_traversal[i], bfs_traversal[i + 1])
-    for i in range(len(dfs_traversal) - 1):
-        dfs_graph.add_edge(dfs_traversal[i], dfs_traversal[i + 1])
-
-    # Display the graphs after traversal
-    print("\nGraph after BFS traversal:")
-    display_graph(bfs_graph.get_adj_list(), title="Graph after BFS traversal")
-    print("\nGraph after DFS traversal:")
-    display_graph(dfs_graph.get_adj_list(), title="Graph after DFS traversal")
-
-
 def main():
     # Create a personalize graph
     g1 = Graph()
